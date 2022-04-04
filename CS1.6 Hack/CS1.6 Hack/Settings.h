c4f938aba73f3626892ec44820030432216fbeff
five5laps <68498669+five5laps@users.noreply.github.com>
2022-04-04 15:30:33 +0300

    [C++]CS 1.6 Hack
    For now there is only Bunnyhop and DDRun hacks
    External, offsets are up to date

---
+++ CS1.6 Hack/CS1.6 Hack/Settings.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace MISC
+{
+	bool bunnyhop = true;
+	bool DDrun    = true;
+}
+
+namespace KEYS
+{
+	int BhopKey     = VK_SPACE;
+	int DDrunKey    = VK_MENU;
+	int QuitKey     = 0x4A;
+}
---
+++ CS1.6 Hack/CS1.6 Hack/include.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//Библиотеки
+#include <iostream>
+#include "MemMan.h"
+#include "procValues.h"
+#include "offsets.h"
+#include "Settings.h"
+#include "Features.h"
+
+//Неймспейсы
