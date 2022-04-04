08c5ec71003cf7235f496ebf49125a8ba3770841
five5laps <68498669+five5laps@users.noreply.github.com>
2022-04-04 15:32:29 +0300

    BunnyHop + DDRun hack
    For now there is only Bunnyhop and DDRun hacks
    External, offsets are up to date

---
+++ [C++]CS1.6 External Hack/CS1.6 Hack/Hack.cpp
@@ -0,0 +1,21 @@
+﻿#include "include.h"
+
+DWORD IsOnGround;
+
+void main()
+{
+    FindProcess();
+    while (true)
+    {
+        IsOnGround = memory.readMem<DWORD>(pv.engineModule + Offsets::OnGround);
+        if(GetAsyncKeyState(KEYS::DDrunKey) && IsOnGround == 1)
+        {
+            DDRun();
+        }
+        if (GetAsyncKeyState(KEYS::BhopKey) && IsOnGround == 1)
+        {
+            BunnyHop();
+        }
+        Sleep(1);
+    }
+}
---
+++ [C++]CS1.6 External Hack/CS1.6 Hack/offsets.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <Windows.h>
+
+namespace Offsets
+{
+	const DWORD OnGround    = 0x122E2D4;
+	const DWORD dwForceJump = 0x131434;
+	const DWORD dwForceDuck = 0x1313B0;
+}
